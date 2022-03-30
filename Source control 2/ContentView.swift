a7a3aba942d1c58c557c51868275bf83038f0285
Wilfie Williams <wilfie4@gmail.com>
2022-03-30 17:46:49 -0400

    Initial Commit

---
+++ Source control 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Source control 2
+//
+//  Created by Wilfie Williams on 30/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source control 2/Source_control_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Source_control_2App.swift
+//  Source control 2
+//
+//  Created by Wilfie Williams on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Source_control_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
